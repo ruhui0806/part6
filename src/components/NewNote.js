6411987823f45d12ba916233d7de09eafa7548be
Ruhui Wen <ruhuiwen0608@gmail.com>
2022-10-30 20:46:13 +0200

    part6-5

--- src/components/NewNote.js
+++ src/components/NewNote.js
@@ -1,15 +1,14 @@
-import { useDispatch } from "react-redux"
+import { connect } from "react-redux"
 import { createNote } from "../reducers/noteReducer"
 
 
-const NewNote = () => {
-    const dispatch = useDispatch()
-
+const NewNote = (props) => {
+    // const dispatch = useDispatch()
     const addNote = async (event) => {
         event.preventDefault()
         const content = event.target.note.value
         event.target.note.value = ''
-        dispatch(createNote(content))
+        props.createNote(content)
     }
 
     return (
@@ -20,4 +19,7 @@ const NewNote = () => {
     )
 }
 
-export default NewNote
+const mapDispatchToProps = { createNote }
+// export default NewNote
+export default connect(null, mapDispatchToProps)(NewNote)//Since the component does not need to access the store's state,
+//export default connect(null, { createNote })(NewNote)//we can simply pass null as the first parameter to connect
--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,4 +1,4 @@
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch, useSelector, connect } from 'react-redux'
 import { toggleImportanceOf } from '../reducers/noteReducer'
 
 const Note = ({ note, handleClick }) => {
@@ -10,31 +10,57 @@ const Note = ({ note, handleClick }) => {
     )
 }
 
-const Notes = () => {
+const Notes = (props) => {
     const dispatch = useDispatch()
-    const notes = useSelector(state => {
-        if (state.filter === 'ALL') {
-            return state.notes
-        }
-        return state.filter === 'IMPORTANT'
-            ? state.notes.filter(note => note.important)
-            : state.notes.filter(note => !note.important)
-    })
+    // const notes = useSelector((state) => {
+    //     if (state.filter === 'ALL') {
+    //         return state.notes
+    //     }
+    //     return state.filter === 'IMPORTANT'
+    //         ? state.notes.filter(note => note.important)
+    //         : state.notes.filter(note => !note.important)
+    // })
+    // const NOTES = props.filter === "ALL" ? props.notes : props.filter === "IMPORTANT" ? props.notes.filter(note => note.important) : props.notes.filter(note => !note.important)
+
+
+
 
     return (
         <ul>
-            {notes.map(note =>
+            {props.notes.map(note =>
                 <Note
                     key={note.id}
                     note={note}
-                    handleClick={() =>
-                        dispatch(toggleImportanceOf(note.id))
+                    handleClick={() => props.toggleImportanceOf(note.id)
                     }
                 />
             )}
         </ul>
     )
 }
+const mapStateToProps = (state) => {
+    if (state.filter === 'ALL') {
+        return {
+            notes: state.notes
+        }
+    }
+    return {
+        notes: (state.filter === 'IMPORTANT'
+            ? state.notes.filter(note => note.important)
+            : state.notes.filter(note => !note.important)
+        )
+    }
+}
 
-export default Notes
+// const mapStateToProps = (state) => {
+//     return {
+//         notes: state.notes,
+//         filter: state.filter
+//     }
+// }
+
+const mapDispatchToProps = { toggleImportanceOf }
+
+const ConnectedNotes = connect(mapStateToProps, mapDispatchToProps)(Notes)
+export default ConnectedNotes
 
