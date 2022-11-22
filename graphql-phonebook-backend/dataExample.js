8e26a41779c76ffbd8b2f43f0f0b3f21e11ac4a9
文静 <kulta@bunseis-MacBook-Air.local>
2022-11-22 21:29:49 +0200

    msg

---
+++ graphql-phonebook-backend/dataExample.js
@@ -0,0 +1,24 @@
+const persons = [
+  {
+    name: 'Arto Hellas',
+    phone: '040-123543',
+    street: 'Tapiolankatu 5 A',
+    city: 'Espoo',
+    id: '3d594650-3436-11e9-bc57-8b80ba54c431',
+  },
+  {
+    name: 'Matti Luukkainen',
+    phone: '040-432342',
+    street: 'Malminkaari 10 A',
+    city: 'Helsinki',
+    id: '3d599470-3436-11e9-bc57-8b80ba54c431',
+  },
+  {
+    name: 'Venla Ruuska',
+    street: 'Nallemäentie 22 C',
+    city: 'Helsinki',
+    id: '3d599471-3436-11e9-bc57-8b80ba54c431',
+  },
+]
+
+module.exports = { persons }
